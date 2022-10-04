8bc068f2c4486616adf8dfd1e10ebee92a4e4e1d
bigjohn98pl <zychaljanusz@gmail.com>
2022-10-04 17:36:32 +0200

    added argument for loop

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 class Witaj2 {
     String powitanie = "Witamy";
 
-    void powitaj() {
-        System.out.println(powitanie);
+    void powitaj(int ile) {
+        for (int i = ile-1; i >= 0 ; i--){
+            System.out.println(powitanie);
+        }
     }
 }
 
@@ -11,6 +13,6 @@ class Witaj {
         System.out.println("Witaj");
 
         Witaj2 mojePowitanie = new Witaj2();
-        mojePowitanie.powitaj();
+        mojePowitanie.powitaj(3);
     }
 }
